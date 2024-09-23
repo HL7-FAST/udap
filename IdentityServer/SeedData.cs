c9ecedf2bdc6624d8abde4495cdae34ffc3f2c8c
Joseph Shook <JoeShook@Gmail.com>
2024-09-23 00:54:39 -0400

    Ensure udap api scope is configured in database

--- IdentityServer/SeedData.cs
+++ IdentityServer/SeedData.cs
@@ -73,13 +73,13 @@ namespace IdentityServer
             //
             // udap
             //
-            //if (context.IdentityResources.All(i => i.Name != UdapConstants.StandardScopes.Udap))
-            //{
-            //    var udapIdentity = new UdapIdentityResources.Udap();
-            //    context.IdentityResources.Add(udapIdentity.ToEntity());
+            if (context.ApiScopes.All(i => i.Name != UdapConstants.StandardScopes.Udap))
+            {
+                var udapIdentity = new UdapApiScopes.Udap();
+                context.ApiScopes.Add(udapIdentity.ToEntity());
 
-            //    await context.SaveChangesAsync();
-            //}
+                await context.SaveChangesAsync();
+            }
 
             //
             // profile
